66ecc3c2ee119d5beab54b3f6fd7d8ca10c9fa91
avca1998 <sfsneed@gmail.com>
2024-10-31 21:50:21 -0700

    initial code

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/_tests_/setup.ts'
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/_tests_/setup.ts",
+  },
+  server: {
+    port: 3000,
+    host: "127.0.0.1",
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        secure: false,
+        changeOrigin: true,
+      },
     },
-    server: {
-        port: 3000,
-        host: '127.0.0.1',
-        open: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3001',
-                secure: false,
-                changeOrigin: true
-            }
-        }
-    }
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/_tests_/setup.ts'
+    environment: "jsdom",
+    setupFiles: "./src/_tests_/setup.ts",
   },
   server: {
     port: 3000,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         secure: false,
-        changeOrigin: true
-      }
-    }
-  }
-})
+        changeOrigin: true,
+      },
+    },
+  },
+});
